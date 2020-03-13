9af058de9a48e8754340798a197a5206ff091b4c
elsonigo <jonas_manser@hotmail.com>
2020-03-14 00:06:54 +0100

    small text changes

--- index.php
+++ index.php
@@ -5,10 +5,10 @@
 	<div x-data="{ open: false }">
 		<button @click="open = true"
 		class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded">
-			Button
+			Tailwind works?
 		</button>
 		<ul x-show="open" @click.away="open = false">
-			Dropdown Body
+			alpinejs works!
 		</ul>
 	</div>
 </section>
